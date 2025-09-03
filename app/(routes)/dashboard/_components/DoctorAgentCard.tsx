89e0f6e26e684123f731b2a290ece9f19fb17852
ARUNDHUTI SARKAR <arundhutisarkar2004@gmail.com>
2025-09-03 11:36:32 +0530

    commit

--- app/(routes)/dashboard/_components/DoctorAgentCard.tsx
+++ app/(routes)/dashboard/_components/DoctorAgentCard.tsx
@@ -2,6 +2,7 @@ import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import React from 'react'
 import { IconArrowRight } from '@tabler/icons-react';
+import AddNewSessionDialog from './AddNewSessionDialog';
 
 export type DoctorAgent = {
     id: number;
@@ -24,7 +25,7 @@ const DoctorAgentCard = ({ doctorAgent }: props) => {
         className='w-full h-[250px] object-cover' />
       <h2 className='font-bold mt-1'>{doctorAgent.specialist}</h2>
       <p className='line-clamp-2 text-sm'>{doctorAgent.description}</p>
-      <Button className='w-full mt-2'>Consultation with me <IconArrowRight /></Button>
+      <div className='w-full mt-2 text-center'><AddNewSessionDialog /></div>
     </div>
   )
 }
